4d84101f0f47c39c1128eff5cecb2a867afb96c0
egwolk <erincovacha@gmail.com>
2025-11-12 04:12:03 +0800

    Add README file, update model name in app.py, enhance docker-compose.yml with openwebui service, and update requirements.txt

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import ollama
-
-model_name = 'inclusion-bot:latest' 
+model_name = 'inclusion-bot'
 
 try:
     stream = ollama.chat(
         model=model_name,
-        messages=[
-            {'role': 'user', 'content': 'hi there!'}
-        ],
+        messages=[{'role': 'user', 'content': 'hi there!'}],
         stream=True
     )
     print(f"successfully connected to {model_name}\n")
@@ -16,7 +13,9 @@ try:
 except ollama.ResponseError as e:
     if e.status_code == 404:
         print(f"Model '{model_name}' not found. Creating from Modelfile...")
-        ollama.create(model_name)
-        print(f"Model '{model_name}' created successfully. Please rerun the script.")
+        with open('Modelfile', 'r', encoding='utf-8') as f:
+            modelfile_text = f.read()
+        ollama.create(model=model_name, modelfile=modelfile_text)
+        print(f"Model '{model_name}' created. Rerun the script.")
     else:
         print(f"An error occurred: {e}")
